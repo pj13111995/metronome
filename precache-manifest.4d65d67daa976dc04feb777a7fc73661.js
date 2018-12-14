e4a865700443c3c62fb3380dabe181b3e4b866db
pj13111995 <praveen.poonja@novigosolutions.com>
2018-12-14 11:59:22 +0530

    Updates

---
+++ precache-manifest.4d65d67daa976dc04feb777a7fc73661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/metronome/static/media/click2.fe0ded65.wav"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "c60a8487983b61cb8a18",
+    "url": "/metronome/static/js/runtime~main.c60a8487.js"
+  },
+  {
+    "revision": "035cbaf27b2ad58b4638",
+    "url": "/metronome/static/js/main.035cbaf2.chunk.js"
+  },
+  {
+    "revision": "1f63d5d7a5b3524b52d9",
+    "url": "/metronome/static/js/1.1f63d5d7.chunk.js"
+  },
+  {
+    "revision": "035cbaf27b2ad58b4638",
+    "url": "/metronome/static/css/main.252a0014.chunk.css"
+  },
+  {
+    "revision": "0c2537941743b99b34869914b105ba37",
+    "url": "/metronome/index.html"
+  }
+];
